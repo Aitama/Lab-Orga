2b0fef6b9d7ca0e323c00bdf855d35dac0db53e7
Aitama <bastian.toro@usach.cl>
2014-10-05 16:24:57 -0300

    Update Taylor2.c

--- Taylor2.c
+++ Taylor2.c
@@ -14,14 +14,15 @@ static double c7 = 1.0/15;
 double Taylor(double y){
 	double x = (y-1);
 	double z = (y+1);
-	return 2 * (c0 * x/z +
-			c1 * x/z* x/z* x/z +
-			c2 * x/z* x/z* x/z* x/z* x/z  +
-			c3 * x/z* x/z* x/z* x/z* x/z* x/z* x/z +
-			c4 * x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z +
-			c5 * x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z +
-			c6 * x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z +
-			c7 * x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z* x/z);
+	double a = x/z;
+	return 2 * (c0 * a +
+			c1 * a* a* a +
+			c2 * a* a* a* a* a  +
+			c3 * a* a* a* a* a* a* a  +
+			c4 * a* a* a* a* a* a* a* a* a +
+			c5 * a* a* a* a* a* a* a* a* a* a* a +
+			c6 * a* a* a* a* a* a* a* a* a* a* a* a* a +
+			c7 * a* a* a* a* a* a* a* a* a* a* a* a* a* a* a);
 }
 
 
@@ -42,11 +43,6 @@ int main(int argc,char *argv[]){
 					}
 					printf("%f\n",ultimo);
 					break;
-			case '?':	
-					break;
-			case '-1':	
-					break;
-			default:
 			
 		}
 	}
